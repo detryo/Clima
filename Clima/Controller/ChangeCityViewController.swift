fb69d922145321a31a760d85afb0309af625f413
Chris <chris.sedano.arenas@gmail.com>
2020-06-09 11:32:16 +0100

    WeatherDataModel.swift, ChangeCityViewController.swift and WeatherViewController.swift
    WeatherDataModel, declare some var and func updateWeatherIcon with switch, ChangeCityViewController.swift, write protocol and config buttons, WeatherViewControllers.swift, Networking func getWeatherData, JSON Parsing func updateWeatherData, UI Updates func updateUIWithWeatherData, Location Manager Delegate Methods and prepare for segue

--- Clima/Controller/ChangeCityViewController.swift
+++ Clima/Controller/ChangeCityViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
+//Write the protocol declaration here:
+protocol ChangeCityDelegate {
+    func userEnteredNewCityName(city: String)
+}
+
 class ChangeCityViewController: UIViewController {
     
+    var delegate: ChangeCityDelegate?
     
+    //This is the pre-linked IBOutlets to the text field:
     @IBOutlet weak var changeCityTextField: UITextField!
-    @IBOutlet weak var getWeatherPressed: UIButton!
-    
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
-    }
-    
-
-    @IBAction func backButtonPressed(_ sender: Any) {
+    //This is the IBAction that gets called when the user taps on the "Get Weather" button:
+    @IBAction func getWeatherPressed(_ sender: UIButton) {
+        
+        let cityName = changeCityTextField.text!
+        delegate?.userEnteredNewCityName(city: cityName)
         dismiss(animated: true, completion: nil)
     }
     
+    //This is the IBAction that gets called when the user taps the back button. It dismisses the ChangeCityViewController.
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
